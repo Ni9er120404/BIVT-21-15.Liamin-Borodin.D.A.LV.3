901a7bb69276f52e859f701623546d3a0abb4f31
Даниил Лямин <Niger120404@yandex.ru>
2023-07-26 16:12:45 +0300

    The framework has been changed

---
+++ LaboratoryWork.OOP.3/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace LaboratoryWork.OOP._3
+{
+	public partial class MainWindow : Window
+	{
+		private readonly List<Student> students;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			students = new List<Student>();
+		}
+
+		private void AddButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (int.TryParse(mathTextBox.Text, out int math)
+				&& int.TryParse(physicsTextBox.Text, out int physics)
+				&& int.TryParse(russianTextBox.Text, out int russian))
+			{
+				float average = (math + physics + russian) / 3f;
+				Student newStudent = new Student(nameTextBox.Text, math, physics, russian, average);
+				students.Add(newStudent);
+				_ = studentsListBox.Items.Add(newStudent);
+			}
+			else
+			{
+				_ = MessageBox.Show("Пожалуйста, введите корректные оценки (целые числа).");
+			}
+		}
+	}
+}
---
+++ LaboratoryWork.OOP.3/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace LaboratoryWork.OOP._3
+{
+	public class Student
+	{
+		public string FullName { get; }
+		public int Math { get; }
+		public int Physics { get; }
+		public int Russian { get; }
+		public float Average { get; }
+
+		public Student(string fullName, int math, int physics, int russian, float average)
+		{
+			FullName = fullName;
+			Math = math;
+			Physics = physics;
+			Russian = russian;
+			Average = average;
+		}
+
+		public override string ToString()
+		{
+			return $"Имя: {FullName}, Математика: {Math}, Физика: {Physics}, Русский: {Russian}, Средний балл: {Average:F2}";
+		}
+	}
+}
